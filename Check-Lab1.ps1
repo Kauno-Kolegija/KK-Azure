b37fdd25acc173fec11d036e970a7006d8e8c731
Mantas Bartkevicius <mantas.bartkevicius@visma.com>
2026-01-30 08:57:57 +0200

    Lab1 fix config

--- Check-Lab1.ps1
+++ Check-Lab1.ps1
@@ -1,6 +1,6 @@
 # --- KONFIGŪRACIJOS GAVIMAS ---
 # Pakeiskite šią nuorodą į savo organizacijos repozitorijos config failo RAW nuorodą
-$configUrl = "https://raw.githubusercontent.com/JUSU_ORG/REPOZITORIJA/main/Check-Lab1-config.json"
+$configUrl = "https://raw.githubusercontent.com/Kauno-Kolegija/KK-Azure/refs/heads/main/Check-Lab1-config.json"
 
 try {
     # Atsisiunčiame ir konvertuojame JSON į PowerShell objektą
