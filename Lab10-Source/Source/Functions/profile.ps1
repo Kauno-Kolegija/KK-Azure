c2305812c3c4d16750fa9529c95f7976f3ed4f1c
Mantas Bartkevicius <mantas.bartkevicius@visma.com>
2026-04-06 13:40:04 +0300

    Remove duplicated files

--- Lab10-Source/Source/Functions/profile.ps1
+++
@@ -1,9 +0,0 @@
-# Azure Functions profile.ps1
-# This runs when the PowerShell worker starts.
-
-if ($env:MSI_SECRET) {
-    Disable-AzContextAutosave -Scope Process | Out-Null
-    # Mes naudojame Managed Identity, bet jūsų atveju connection string veiks ir be šito.
-    # Tačiau šis failas padeda išvengti klaidų ateityje.
-    Connect-AzAccount -Identity
-}
